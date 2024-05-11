de9858d06625671978f681d072e8a6d2183f2f6e
Sidnei <neimestre@yahoo.com.br>
2024-05-11 20:05:52 -0300

    updated sidebar header

--- src/components/Sidebar/components/SideHeader/index.tsx
+++ src/components/Sidebar/components/SideHeader/index.tsx
@@ -1,9 +1,10 @@
 import { BxCodeAlt } from "../../../../assets/icons/BxCodeAlt";
+import "./styles.css";
 
 export default function SideHeader() {
   return (
     <div className="side-header">
-      <a href="#" className="logo">
+      <a href="#">
         <BxCodeAlt />
         <pre>
           <span>D</span>
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,6 +3,7 @@ import SideHeader from "./components/SideHeader";
 import SideBody from "./components/SideBody";
 import { sideItems } from "./components/sideItems";
 import SideFooter from "./components/SideFooter";
+import "./styles.css";
 
 export default function Sidebar(props: ISidebarProps) {
   return (
