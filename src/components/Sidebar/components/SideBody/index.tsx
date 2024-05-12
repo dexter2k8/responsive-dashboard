1811f57733cba6ba703437d537fb370da79e5f93
Sidnei <neimestre@yahoo.com.br>
2024-05-11 21:58:53 -0300

    updated sidebar styles

--- src/components/Sidebar/components/SideBody/index.tsx
+++ src/components/Sidebar/components/SideBody/index.tsx
@@ -1,11 +1,14 @@
 import { ISideBodyProps } from "./types";
+import "./styles.css";
+import { useState } from "react";
 
 export default function SideBody({ items }: ISideBodyProps) {
+  const [active, setActive] = useState(0);
   return (
     <ul className="side-body">
       {items.map(({ icon, title }, i) => (
-        <li key={i}>
-          <a href="#">
+        <li key={i} className={active === i ? "active" : ""}>
+          <a href="#" onClick={() => setActive(i)}>
             {icon} {title}
           </a>
         </li>
