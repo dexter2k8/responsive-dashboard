07d6983f34b5d4b6e24682b8a37542c9fffe3779
Sidnei <neimestre@yahoo.com.br>
2024-05-14 00:28:02 -0300

    updated collapse footer

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,9 +1,9 @@
+import "./styles.css";
 import { ISidebarProps } from "./types";
 import SideHeader from "./components/SideHeader";
 import SideBody from "./components/SideBody";
 import { sideItems } from "./components/sideItems";
 import SideFooter from "./components/SideFooter";
-import "./styles.css";
 
 export default function Sidebar({ collapsed }: ISidebarProps) {
   const sidebarClass = `sidebar ${collapsed ? "collapsed" : ""}`;
